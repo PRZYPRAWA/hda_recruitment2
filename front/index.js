31f5c1c05c2c916cec0874164b847fed826f06e8
Pawel <mrprzyprawa@gmail.com>
2020-11-23 01:31:37 +0100

    Refactor search

--- front/index.js
+++ front/index.js
@@ -1,21 +1,25 @@
-const url = "http://localhost:8000/users/";
 const searchInput = document.querySelector(".search");
 const suggestions = document.querySelector(".suggestions");
+const url = "http://localhost:8000/users/";
 
 function fetchMatches(word, url, suggestions) {
-  fetch(url + word)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      const matches = data
-        .map(
-          (person) =>
-            `<li><span class="name">${person.name} - ${person.age}</span></li>`
-        )
-        .join("");
-      suggestions.innerHTML = matches;
-    })
-    .catch((err) => console.error(err));
+  if (word) {
+    fetch(url + word)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const matches = data
+          .map(
+            (person) =>
+              `<li><span class="name">${person.name} - ${person.age}</span></li>`
+          )
+          .join("");
+        suggestions.innerHTML = matches;
+      })
+      .catch((err) => console.error(err));
+  } else {
+    suggestions.innerHTML = `<li><span class="name">Nothing to show</span></li>`;
+  }
 }
 
 function displayMatches() {
