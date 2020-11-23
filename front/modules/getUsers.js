3c3e93a01d73b5cd32cdbeb04b8843a399802851
Pawel <mrprzyprawa@gmail.com>
2020-11-23 12:32:48 +0100

    Refactor showing list item

--- front/modules/getUsers.js
+++ front/modules/getUsers.js
@@ -1,6 +1,8 @@
 import { url } from "./consts.js";
 const suggestions = document.querySelector(".suggestions");
 
+const userListItem = (info) => `<li><span class="name">${info}</span></li>`;
+
 function getUsers(word, url, suggestions) {
   if (word) {
     fetch(url + word)
@@ -9,19 +11,16 @@ function getUsers(word, url, suggestions) {
         console.log(data);
         if (data.length !== 0) {
           const matches = data
-            .map(
-              (person) =>
-                `<li><span class="name">${person.name} - ${person.age}</span></li>`
-            )
+            .map((person) => userListItem(`${person.name} - ${person.age}`))
             .join("");
           suggestions.innerHTML = matches;
         } else {
-          suggestions.innerHTML = `<li><span class="name">Nothing found</span></li>`;
+          suggestions.innerHTML = userListItem("Nothing found");
         }
       })
       .catch((err) => console.error(err));
   } else {
-    suggestions.innerHTML = `<li><span class="name">Nothing to show</span></li>`;
+    suggestions.innerHTML = userListItem("Nothing to show");
   }
 }
 
